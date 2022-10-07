6c04aa0498eaa1259449774c9a3da7d9383cb28e
hanxuran <hxr@atguigu.com>
2022-10-07 14:04:13 +0800

    second commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("git-test111");
+        System.out.println("git-test222");
     }
 }
